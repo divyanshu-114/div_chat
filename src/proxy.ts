e05651d364a60cf48d836aa6cb30233969831248
Divyanshu Raj <divyanshu.singh.1879@gmail.com>
2026-02-02 00:59:46 +0530

    f

--- src/proxy.ts
+++ src/proxy.ts
@@ -38,7 +38,7 @@ export const proxy = async (req: NextRequest) => {
     path: "/",
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "lax",
   })
 
   await redis.hset(`meta:${roomId}`, {
